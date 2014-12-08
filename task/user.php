972a9abbeb316f914d201ae6e8cad1ef45cfa49f
Excalion <1excalion@gmail.com>
2014-12-09 01:15:46 +0200

    Create user.php

---
+++ task/user.php
@@ -0,0 +1,12 @@
+<?php
+abstract class User {
+  look ( ) ;
+  get Car ( car_id ) ;
+  }
+class Auto {
+  public status ; // б/у или новая
+  public type ;   // легковая или грузовая
+  public function sell ( car_id ) {
+    if ( this car )
+    }
+  }
